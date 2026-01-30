6248203a6608779a32281c2d51de9e940e3b153b
Lim Hong Yi <yeegolim@gmail.com>
2026-01-30 17:46:11 +0800

    Day 2: data processing loop flow

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 def main():
-	name = input("Please enter your name: ")
-	raw_age = input("Please enter your age: ")
 
-	if raw_age.isdigit():
-		age = int(raw_age)
-		print("Hello,", name)
-		print("Next year you will be:", age + 1)
-	else:
-		print("Invalid age input. Please enter a number.")
+	users = [
+		{"name": "Alice", "age": 15, "active": True} ,
+		{"name": "Bob", "age": 22, "active": True},
+		{"name": "Charlie", "age": 30, "active": True},
+		{"name": "Daisy", "age": 17, "active": True},
+		{"name": "Eve", "age": 40, "active": False},
+	]
+
+	print("Active adult users:")
+	found_underage_active = False	
+
+	for index, user in enumerate(users, start=1):
+
+		if not user["active"]:
+			continue
+
+		if user["age"] < 18:
+			found_underage_active = True
+			continue
 		
+		print(index, user["name"], user["age"])		
 
+	if found_underage_active:
+		print("Warning: There is at least one underage active user!")
+			
 
 
 if __name__ == "__main__":
